206b8bd2e864ef57a6e98faa9b6fc81c39935224
Kirill Pogartsev <kirill.pogartsev@ya.ru>
2021-09-21 06:27:51 +0000

    fix: удалены ненужные файлы и ноды

--- fuel_planner/bag/debug_record.sh
+++
@@ -1,1 +0,0 @@
-rosbag record /vins_estimator/imu_propagate /camera/depth/image_rect_raw /vins_estimator/camera_pose
--- fuel_planner/bag/explore_record.sh
+++
@@ -1,3 +0,0 @@
-# camera, map, range, path, traj, cmd
-# /vins_estimator/camera_pose
-rosbag record /planning_vis/frontier /planning_vis/viewpoints /sdf_map/occupancy_local /sdf_map/occupancy_all /planning_vis/trajectory /planning/travel_traj /planning/position_cmd_vis /planning/pos_cmd
