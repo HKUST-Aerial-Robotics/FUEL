82c9304b9e75570d56f5f5c29e679c075f201967
yunhajo <yjo@caltech.edu>
2024-07-02 10:20:21 -0700

    fixed sudden crash using 2d nav goal issue

--- fuel_planner/path_searching/src/kinodynamic_astar.cpp
+++ fuel_planner/path_searching/src/kinodynamic_astar.cpp
@@ -650,7 +650,7 @@ Eigen::Vector3i KinodynamicAstar::posToIndex(Eigen::Vector3d pt) {
 }
 
 int KinodynamicAstar::timeToIndex(double time) {
-  int idx = floor((time - time_origin_) * inv_time_resolution_);
+  return floor((time - time_origin_) * inv_time_resolution_);
 }
 
 void KinodynamicAstar::stateTransit(Eigen::Matrix<double, 6, 1>& state0,
